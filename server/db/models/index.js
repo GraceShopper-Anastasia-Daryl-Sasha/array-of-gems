33bb21f456d486d3c7d88ef84b21d11892e3e7e6
DC-1680 <dbconcha@gmail.com>
2018-07-24 14:38:39 -0400

    updated product model, removed cateogry and tag models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,13 +2,9 @@ const User = require('./user');
 const Order = require('./order');
 const Product = require('./product');
 const Review = require('./review');
-const Category = require('./category');
-const Tag = require('./tag');
 
 // - Orders must belong to a user OR guest session (authenticated vs unauthenticated)
 
-Category.hasMany(Tag, { foreignKey: 'CategoryTag' })
-Tag.belongsTo(Category, { foreignKey: 'CategoryTag' })
 
 Review.belongsTo(Product, { foreignKey: 'ReviewProductId' })
 Product.hasMany(Review, { foreignKey: 'ReviewProductId' })
@@ -19,8 +15,6 @@ User.hasMany(Review, { foreignKey: 'ReviewUserId' })
 Order.belongsToMany(Product, { through: 'OrderProducts' })
 Product.belongsToMany(Order, { through: 'OrderProducts' })
 
-Tag.hasMany(Product, { foreignKey: 'ProductTag' })
-Product.belongsTo(Tag, { foreignKey: 'ProductTag' })
 
 User.hasMany(Order)
 
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -25,6 +25,15 @@ const Product = db.define('product', {
     photos: {
         type: Sequelize.ARRAY(Sequelize.STRING),
         defaultValue: [['https://cdn.tutsplus.com/vector/uploads/2014/03/0a_Gems_tutorial_ruby.jpg']]
+    },
+    size: {
+        type: Sequelize.ENUM('Birthstone', 'Raw', 'Polished')
+    },
+    type: {
+        type: Sequelize.ENUM('0.5 mm', '1.0 mm', '1.25 mm', '1 in', '2 in')
+    },
+    color: {
+        type: Sequelize.ENUM('Blue', 'Green', 'Purple', 'Red', 'Yellow', 'Black', 'Pink')
     }
 
 })
