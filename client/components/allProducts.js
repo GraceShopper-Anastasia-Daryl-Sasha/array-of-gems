848159845b4527fc2345491d1bd1db456ccab960
DC-1680 <dbconcha@gmail.com>
2018-07-25 11:14:50 -0400

    added search bar to nav

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import Sidebar from './sidebar'
 
 export default class AllProducts extends Component {
-  render () {
+  render() {
     const products = this.props.allProducts
     return (
       //put onclick listener on each product, which is a react link to the singleProduct container, the container will have a selectedProduct dispatch action to the store to update the selected product to the one clicked, then singleProduct presentational component will render the selected product, onclick listener with event handler that will dispatch the action instead
-      <div>
+      <div className="main-container">
         <Sidebar />
         <div>
-        <ul>
+          <ul>
             {
-            products.map(product => (
-              <li key={product.id}>{product.title}</li>
-            ))
-          }
+              products.map(product => (
+                <li key={product.id}>{product.title}</li>
+              ))
+            }
 
-        </ul>
+          </ul>
         </div>
       </div>
     )
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from '../store'
 
+import SearchBar from './searchBar'
 
 // include cart 
 // my Account if logged in
@@ -12,26 +13,32 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
 
     <nav>
+
+
       <div className="logo">
         <img src={'http://rgs.usu.edu/rd/wp-content/uploads/sites/17/2017/02/61326_154623_ruby_diamond_gem-300x300.png'} />
         <h1><Link to="/home">Array Of Gems</Link></h1>
+        <h3><Link to='/products'>All Products</Link></h3>
+        <SearchBar />
       </div>
       {isLoggedIn ? (
         <div className="navLinks">
           {/* The navbar will show these links after you log in */}
-          <img src={'https://banner2.kisspng.com/20171217/337/shopping-cart-png-5a364b75338266.010470061513507701211.jpg'} />
-          <Link to="/home">Home</Link>
+          <Link to="/myAccount">My Account</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          <img src={'https://banner2.kisspng.com/20171217/337/shopping-cart-png-5a364b75338266.010470061513507701211.jpg'} />
+          <span>0</span>
         </div>
       ) : (
           <div className="navLinks">
             {/* The navbar will show these links before you log in */}
-            <img src={'http://simpleicon.com/wp-content/uploads/Shopping-Cart-10.png'} />
-            <span>0</span>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <img src={'http://simpleicon.com/wp-content/uploads/Shopping-Cart-10.png'} />
+            <span>0</span>
+
           </div>
         )}
     </nav>
