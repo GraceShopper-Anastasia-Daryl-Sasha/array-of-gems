c7ce8425e37c83e688b1229bc951eb8036b658bc
Alexandra Havia <sasha.havia@gmail.com>
2018-07-24 21:51:37 -0400

    Implemented Sidebar component

--- client/app.js
+++ client/app.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import { Navbar } from './components'
 import Routes from './routes'
+import Sidebar from './components/sidebar'
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<Routes />
+			<Sidebar />
+		</div>
+	)
 }
 
 export default App
