faf246629362384980285178958bcfd39355e6e1
Anastasia Garelli <anastasia.garelli@gmail.com>
2018-07-25 17:18:48 -0400

    Updated Single Product Component

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Sidebar from './sidebar'
+import { Link } from 'react-router-dom'
 
 export default class AllProducts extends Component {
 	render() {
@@ -8,9 +9,24 @@ export default class AllProducts extends Component {
 			<div className="main-container">
 				<Sidebar products={products} categories={categories} />
 				<div>
-					<ul>
-						{products.map(product => <li key={product.id}>{product.title}</li>)}
-					</ul>
+				{
+                        products.map(product => {
+                            return (
+                                <div className="product" key={product.id}>
+                                    <div className="product-image">
+                                        <img src={product.photo} />
+                                    </div>
+                                    <div className="product-info">
+                                        <Link to={`/products/${product.id}`}>{product.title}</Link> <br />
+                                        <a>Price: ${product.price}</a>
+                                    </div>
+                                    <div className='product-buttons'>
+                                        <button>Add to Cart</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
 				</div>
 			</div>
 		)
