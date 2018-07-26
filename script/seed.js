5d4a1fee72a856d696bb904764504170684d3b70
Alexandra Havia <sasha.havia@gmail.com>
2018-07-26 12:24:50 -0400

    Update(seed file) Seed file creates photos

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const { User, Review, Product } = require('../server/db/models')
+const { User, Review, Product, Photo } = require('../server/db/models')
 
 /**
  * Welcome to the seed file! This seed file uses a newer language feature called...
@@ -106,10 +106,6 @@ const products = [
 			'This beautiful stone, which is most commonly red but can be found in a range of other colors, symbolizes peace, prosperity and good health.',
 		price: 6.95,
 		quantity: 25,
-		photos: [
-			'https://img.etsystatic.com/il/ad6e2b/1455198496/il_570xN.1455198496_sp58.jpg?version=1',
-			'https://img.etsystatic.com/il/eddf57/1455198532/il_570xN.1455198532_2rgd.jpg?version=0'
-		],
 		color: 'Green',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -120,10 +116,6 @@ const products = [
 			'Amethyst is a well known mineral and gemstone. It is the purple variety of the mineral Quartz, and its most valuable and prized variety. Its name derives from the Greek "amethystos", which means "not drunken", as Amethyst in antiquity was thought to ward off drunkenness.',
 		price: 5.55,
 		quantity: 72,
-		photos: [
-			'https://img.etsystatic.com/il/5fa555/1374425596/il_570xN.1374425596_9alc.jpg?version=1',
-			'https://img.etsystatic.com/il/5fa555/1374425596/il_570xN.1374425596_9alc.jpg?version=1'
-		],
 		color: 'Purple',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -134,10 +126,6 @@ const products = [
 			'Aquamarine is the greenish-blue to blue variety of Beryl. Unlike Emerald which usually is flawed or heavily included, Aquamarine can form in stunning flawless crystals, creating some of the most beautiful mineral masterpieces.',
 		price: 14.5,
 		quantity: 15,
-		photos: [
-			'https://i.etsystatic.com/iap/46ee8b/1604051835/iap_300x300.1604051835_jvyrugke.jpg?version=0',
-			'https://i.etsystatic.com/iap/46ee8b/1604051835/iap_300x300.1604051835_jvyrugke.jpg?version=0'
-		],
 		color: 'Blue',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -148,10 +136,6 @@ const products = [
 			'The Precious Gemstone Diamond. Renowned for being the hardest substance on earth, its sparkling fire, durability, and rarity make Diamond the most prized of all gems. No gemstone contains as much allure and interest as does Diamond',
 		price: 270.75,
 		quantity: 7,
-		photos: [
-			'https://www.thediamondloupe.com/sites/awdcnewswall/files/Screen%20Shot%202016-09-09%20at%2013.07.44.png',
-			'https://cdn.shopify.com/s/files/1/0851/2360/products/DSCF0736.jpg?v=1510750618'
-		],
 		color: 'White',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -162,11 +146,6 @@ const products = [
 			'Emerald is the most precious stone in the beryl group. The name comes from the old French word "esmeralde", which was derived from the Greek word "smaragdos" meaning "green stone".',
 		price: 18.75,
 		quantity: 21,
-		photos: [
-			'https://img.etsystatic.com/il/c5d275/1372882974/il_570xN.1372882974_88v6.jpg?version=0',
-			'https://img.etsystatic.com/il/ce1e0f/1420152195/il_570xN.1420152195_9h0n.jpg?version=0',
-			'https://i.etsystatic.com/6245839/r/il/e04d7c/1106499987/il_570xN.1106499987_5b4p.jpg'
-		],
 		color: 'Green',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -177,10 +156,6 @@ const products = [
 			'Ruby is a pink to blood-red colored gemstone, a variety of the mineral corundum. Other varieties of gem-quality corundum are called sapphires.',
 		price: 15.72,
 		quantity: 27,
-		photos: [
-			'http://cdn.shopify.com/s/files/1/0825/7459/files/ruby_raw_large.jpg?11116142272450580674',
-			'http://cdn.shopify.com/s/files/1/0825/7459/files/ruby_raw_large.jpg?11116142272450580674'
-		],
 		color: 'Red',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -191,10 +166,6 @@ const products = [
 			'Peridot is one of the few gemstones that occur in only one color: an olive-green. The intensity and tint of the green, however, depends on the percentage of iron that is contained in the crystal structure, so the color of individual peridot gems can vary from yellow, to olive, to brownish-green.',
 		price: 4.2,
 		quantity: 39,
-		photos: [
-			'https://i.etsystatic.com/7879975/r/il/844cdb/1084234876/il_570xN.1084234876_dvt3.jpg',
-			'https://i.etsystatic.com/7879975/r/il/78a64b/1084234888/il_570xN.1084234888_8xp2.jpg'
-		],
 		color: 'Green',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -205,10 +176,6 @@ const products = [
 			'The sapphire is the birthstone for the month of September. The name sapphire is derived from the Latin word ““saphirus” and the Greek word “sapheiros,” both meaning blue. Some believe that the name sapphire is derived from its association with the planet Saturn. The name can be roughly be translated to mean “dear to the planet Saturn” in many different languages.',
 		price: 3.75,
 		quantity: 52,
-		photos: [
-			'http://www.vettrigemsusa.com/image/cache/data/0Sapphire/zzzl111-800x800.jpg',
-			'https://www.worthy.com/blog/wp-content/uploads/2014/06/TitanicHeart-of-the-Ocean.jpg'
-		],
 		color: 'Blue',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -219,10 +186,6 @@ const products = [
 			'The beautiful opal is formed from rain. There is some conjecture on how exactly this precious gemstone forms, but many believe it is formed when water from rain seeps down into crevasses in the rock. Once the water evaporates, the silica that is left behind dries out and hardens into precious opal.',
 		price: 9.25,
 		quantity: 38,
-		photos: [
-			'https://www.antiqworld.com/blog/wp-content/uploads/2017/11/opal-833x713.png',
-			'https://www.antiqworld.com/blog/wp-content/uploads/2017/11/opal-833x713.png'
-		],
 		color: 'Black',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -233,10 +196,6 @@ const products = [
 			'Most unadulterated topaz is colorless or pale blue. The most rare and valuable topaz is yellow, or pink to reddish-orange, and is known as "imperial topaz" or "precious topaz". Some yellowish-brown topaz gems can gradually fade when continually exposed to daylight.',
 		price: 17.5,
 		quantity: 64,
-		photos: [
-			'https://img.etsystatic.com/il/035732/856848544/il_570xN.856848544_cobh.jpg?version=1',
-			'https://img.etsystatic.com/il/035732/856848544/il_570xN.856848544_cobh.jpg?version=1'
-		],
 		color: 'Yellow',
 		size: '1.0 mm',
 		type: 'Birthstone'
@@ -247,11 +206,6 @@ const products = [
 			'A Black Onyx stone provides powerful protective energy that help to shield the mind and body from negative energies and electromagnetic energies. It absorbs and transforms negative energy, while preventing the draining of personal energy in the process.',
 		price: 5.75,
 		quantity: 67,
-		photos: [
-			'https://i.etsystatic.com/16996458/r/il/cf5cab/1595754747/il_570xN.1595754747_e9a3.jpg',
-			'https://i.etsystatic.com/16996458/r/il/e4d86c/1548298034/il_570xN.1548298034_aysl.jpg',
-			'https://i.etsystatic.com/10506764/r/il/72bad7/1229973179/il_570xN.1229973179_1smi.jpg'
-		],
 		color: 'Black',
 		size: '1.0 mm',
 		type: 'Polished'
@@ -262,10 +216,6 @@ const products = [
 			'Rose Quartz is the rosy pink variety of Quartz. Its color is usually soft, ranging from very light pink to medium pink in intensity. It is often hazy or turbid, which makes it lack good transparency.',
 		price: 3.45,
 		quantity: 10,
-		photos: [
-			'https://img.etsystatic.com/il/c8511a/1566584228/il_570xN.1566584228_kxah.jpg?version=0',
-			'https://img.etsystatic.com/il/c2135c/1614038293/il_570xN.1614038293_j6x8.jpg?version=0'
-		],
 		color: 'Pink',
 		size: '1.0 mm',
 		type: 'Raw'
@@ -276,10 +226,6 @@ const products = [
 			'Dendritic agate is a translucent, colorless to whitish-gray variety of chalcedony quartz, easily distinguished by its distinct tree- or fern-like markings known as "dendrites", which are most often brown to black in color.',
 		price: 15.23,
 		quantity: 3,
-		photos: [
-			'https://img.etsystatic.com/il/c8511a/1566584228/il_570xN.1566584228_kxah.jpg?version=0',
-			'https://img.etsystatic.com/il/c2135c/1614038293/il_570xN.1614038293_j6x8.jpg?version=0'
-		],
 		color: 'Brown',
 		size: '1.0 mm',
 		type: 'Polished'
@@ -290,10 +236,6 @@ const products = [
 			'Zoisite is a mineral that includes several gem varieties. The most important and well-known is Tanzanite, a sensational blue gemstone. The variety Thulite is used as a minor pink gemstone. A variety known as Ruby Zoisite, which is green Zoisite associated with opaque red Ruby (and often black amphibole streaks), is used as a carving gemstone as well as a minor gem.',
 		price: 6.78,
 		quantity: 8,
-		photos: [
-			'https://i.ebayimg.com/images/g/cLgAAOSwgY9Xe1BA/s-l300.jpg',
-			'https://i.ebayimg.com/images/g/cLgAAOSwgY9Xe1BA/s-l300.jpg'
-		],
 		color: 'Green',
 		size: '1.0 mm',
 		type: 'Raw'
@@ -304,10 +246,6 @@ const products = [
 			'Besides the classical red/brown lava rock, we will dive in other alternative materials used for fire pits with similar accents such as the black lava rock, black lava glass or obsidian, reflective fire glass, natural fire glass and landscape glass.',
 		price: 2.35,
 		quantity: 67,
-		photos: [
-			'https://i.etsystatic.com/14093570/r/il/af44b5/1365144441/il_570xN.1365144441_lzyo.jpg',
-			'https://img.etsystatic.com/il/2642ad/1132141037/il_570xN.1132141037_pslj.jpg?version=0'
-		],
 		color: 'Blue',
 		size: '1.0 mm',
 		type: 'Raw'
@@ -318,10 +256,6 @@ const products = [
 			'Jasper is known as the Stone of Endurance. It’s a gentle, but vital stimulator of chi which brings physical strength, energy, stamina, focus, and determination. Its steady frequency calms the emotional body creating a lasting and stable energy for setting and completing goals, facing unpleasant tasks, and having the courage to right wrongs.',
 		price: 35.2,
 		quantity: 17,
-		photos: [
-			'https://i.etsystatic.com/12396175/r/il/7fa548/1464057553/il_570xN.1464057553_sipt.jpg',
-			'https://i.etsystatic.com/12396175/r/il/78d8a5/1416791084/il_570xN.1416791084_tu2p.jpg'
-		],
 		color: 'Blue',
 		size: '1.0 mm',
 		type: 'Polished'
@@ -332,13 +266,180 @@ const products = [
 			'Moldavite, also known as the "Bouteille Stone" or as "Vltavin" in Czech, is an olive-green to dull brown-green vitreous gemstone. Moldavite was thought to have been formed by condensed rock vapors after a meteorite impact.',
 		price: 22.15,
 		quantity: 13,
-		photos: [
-			'https://i.etsystatic.com/14050262/r/il/495040/1465610658/il_570xN.1465610658_erbz.jpg',
-			'https://i.etsystatic.com/14050262/r/il/da2675/1512864927/il_570xN.1512864927_sy91.jpg'
-		],
 		color: 'Green',
 		size: '1.0 mm',
 		type: 'Raw'
+	}
+]
+
+const photos = [
+	{
+		image:
+			'https://img.etsystatic.com/il/ad6e2b/1455198496/il_570xN.1455198496_sp58.jpg?version=1',
+		productId: 1
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/eddf57/1455198532/il_570xN.1455198532_2rgd.jpg?version=0',
+		productId: 1
+	},
+	{
+		image:
+			'https://i.etsystatic.com/iap/46ee8b/1604051835/iap_300x300.1604051835_jvyrugke.jpg?version=0',
+		productId: 3
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/5fa555/1374425596/il_570xN.1374425596_9alc.jpg?version=1',
+		productId: 2
+	},
+	{
+		image:
+			'https://i.etsystatic.com/iap/46ee8b/1604051835/iap_300x300.1604051835_jvyrugke.jpg?version=0',
+		productId: 2
+	},
+	{
+		image:
+			'https://www.thediamondloupe.com/sites/awdcnewswall/files/Screen%20Shot%202016-09-09%20at%2013.07.44.png',
+		productId: 4
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/c5d275/1372882974/il_570xN.1372882974_88v6.jpg?version=0',
+		productId: 5
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/ce1e0f/1420152195/il_570xN.1420152195_9h0n.jpg?version=0',
+		productId: 5
+	},
+	{
+		image:
+			'https://i.etsystatic.com/6245839/r/il/e04d7c/1106499987/il_570xN.1106499987_5b4p.jpg',
+		productId: 5
+	},
+	{
+		image:
+			'http://cdn.shopify.com/s/files/1/0825/7459/files/ruby_raw_large.jpg?11116142272450580674',
+		productId: 6
+	},
+	{
+		image:
+			'http://cdn.shopify.com/s/files/1/0825/7459/files/ruby_raw_large.jpg?11116142272450580674',
+		productId: 6
+	},
+	{
+		image:
+			'https://i.etsystatic.com/7879975/r/il/844cdb/1084234876/il_570xN.1084234876_dvt3.jpg',
+		productId: 7
+	},
+	{
+		image:
+			'https://i.etsystatic.com/7879975/r/il/78a64b/1084234888/il_570xN.1084234888_8xp2.jpg',
+		productId: 7
+	},
+	{
+		image:
+			'http://www.vettrigemsusa.com/image/cache/data/0Sapphire/zzzl111-800x800.jpg',
+		productId: 8
+	},
+	{
+		image:
+			'https://www.worthy.com/blog/wp-content/uploads/2014/06/TitanicHeart-of-the-Ocean.jpg',
+		productId: 8
+	},
+	{
+		image:
+			'https://www.antiqworld.com/blog/wp-content/uploads/2017/11/opal-833x713.png',
+		productId: 9
+	},
+	{
+		image:
+			'https://www.antiqworld.com/blog/wp-content/uploads/2017/11/opal-833x713.png',
+		productId: 9
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/035732/856848544/il_570xN.856848544_cobh.jpg?version=1',
+		productId: 10
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/035732/856848544/il_570xN.856848544_cobh.jpg?version=1',
+		productId: 10
+	},
+	{
+		image:
+			'https://i.etsystatic.com/16996458/r/il/cf5cab/1595754747/il_570xN.1595754747_e9a3.jpg',
+		productId: 11
+	},
+	{
+		image:
+			'https://i.etsystatic.com/16996458/r/il/e4d86c/1548298034/il_570xN.1548298034_aysl.jpg',
+		productId: 11
+	},
+	{
+		image:
+			'https://i.etsystatic.com/10506764/r/il/72bad7/1229973179/il_570xN.1229973179_1smi.jpg',
+		productId: 11
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/c8511a/1566584228/il_570xN.1566584228_kxah.jpg?version=0',
+		productId: 12
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/c2135c/1614038293/il_570xN.1614038293_j6x8.jpg?version=0',
+		productId: 12
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/c8511a/1566584228/il_570xN.1566584228_kxah.jpg?version=0',
+		productId: 13
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/c2135c/1614038293/il_570xN.1614038293_j6x8.jpg?version=0',
+		productId: 13
+	},
+	{
+		image: 'https://i.ebayimg.com/images/g/cLgAAOSwgY9Xe1BA/s-l300.jpg',
+		productId: 14
+	},
+	{
+		image: 'https://i.ebayimg.com/images/g/cLgAAOSwgY9Xe1BA/s-l300.jpg',
+		productId: 14
+	},
+	{
+		image:
+			'https://i.etsystatic.com/14093570/r/il/af44b5/1365144441/il_570xN.1365144441_lzyo.jpg',
+		productId: 15
+	},
+	{
+		image:
+			'https://img.etsystatic.com/il/2642ad/1132141037/il_570xN.1132141037_pslj.jpg?version=0',
+		productId: 15
+	},
+	{
+		image:
+			'https://i.etsystatic.com/12396175/r/il/7fa548/1464057553/il_570xN.1464057553_sipt.jpg',
+		productId: 16
+	},
+	{
+		image:
+			'https://i.etsystatic.com/12396175/r/il/78d8a5/1416791084/il_570xN.1416791084_tu2p.jpg',
+		productId: 16
+	},
+	{
+		image:
+			'https://i.etsystatic.com/14050262/r/il/495040/1465610658/il_570xN.1465610658_erbz.jpg',
+		productId: 17
+	},
+	{
+		image:
+			'https://i.etsystatic.com/14050262/r/il/da2675/1512864927/il_570xN.1512864927_sy91.jpg',
+		productId: 17
 	}
 ]
 
@@ -365,6 +466,7 @@ async function seed() {
 
 	await Promise.all(products.map(product => Product.create(product)))
 	await Promise.all(reviews.map(review => Review.create(review)))
+	await Promise.all(photos.map(photo => Photo.create(photo)))
 	// Wowzers! We can even `await` on the right-hand side of the assignment operator
 	// and store the result that the promise resolves to in a variable! This is nice!
 	console.log(`seeded ${users.length} users`)
