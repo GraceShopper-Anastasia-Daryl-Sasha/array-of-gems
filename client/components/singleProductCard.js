0d1389ce3052559a605f1f2b4bae42523e6c811f
DC-1680 <dbconcha@gmail.com>
2018-07-27 13:12:08 -0400

    Fixed (singleProduct): Fixed merged conflict

--- client/components/singleProductCard.js
+++ client/components/singleProductCard.js
@@ -1,46 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-<<<<<<< HEAD
-const SingleProductCard = props => {
+const SingleProductCard = (props) => {
 	const { product } = props
+
+
 	return (
 		<div className="product">
 			<div className="product-image">
-				<img src={product.photos[0].image} />
+				<img src={product.photos[0]} />
 			</div>
 			<div className="product-info">
 				<a>{product.title}</a> <br />
 				<a>Price: ${product.price}</a>
 			</div>
-			<div className="product-buttons">
-				<button>Add to Cart</button>
-				<button>
-					<Link to={`/products/${product.id}`}>More Info</Link>
-				</button>
+			<div className='product-buttons'>
+				<button><Link to={`/products/${product.id}`}>More Info</Link></button>
 			</div>
 		</div>
 	)
-=======
-const SingleProductCard = (props) => {
-    const { product } = props
-
-
-    return (
-        <div className="product">
-            <div className="product-image">
-                <img src={product.photos[0]} />
-            </div>
-            <div className="product-info">
-                <a>{product.title}</a> <br />
-                <a>Price: ${product.price}</a>
-            </div>
-            <div className='product-buttons'>
-                <button><Link to={`/products/${product.id}`}>More Info</Link></button>
-            </div>
-        </div>
-    )
->>>>>>> master
 }
 
 export default SingleProductCard
